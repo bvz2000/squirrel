54ee99d3d7530ed868ac5c5623451b7aeac8df06
bvz <bvz@c381.com>
2019-08-22 23:46:04 -0700

    Added a set_attributes method in place of setting attrs on object creation

--- modules/interface/storeinterface.py
+++ modules/interface/storeinterface.py
@@ -17,85 +17,35 @@ class StoreInterface(object):
 
     # --------------------------------------------------------------------------
     def __init__(self,
-                 name,
-                 asset_parent_d,
-                 src_p=None,
-                 metadata=None,
-                 keywords=None,
-                 notes=None,
-                 thumbnails=None,
-                 poster_frame=None,
-                 merge=True,
-                 pins=None,
-                 verify_copy=False,
                  language="english"):
-        """
-        Initialize the interface.
 
-        :param name: The name of the thing we are storing. Note that this is not
-               necessarily the file name of the thing we are storing (though you
-               could use the file name if desired).
-        :param asset_parent_d: The full path to where the source directory will
-               be copied to OR the full path to the parent directory of an
-               existing asset we will be modifying.
-        :param src_p: A full path to the directory OR file being copied. May be
-               set to None for cases where we are modifying an existing asset.
-               Defaults to None.
-        :param metadata: A dictionary of key, value pairs that defines arbitrary
-               metadata for this asset. If None, no metadata will be stored.
-               Defaults to None.
-        :param keywords: An optional list of keywords. If None, no keywords will
-               be stored. Defaults to None.
-        :param notes: An optional string containing free-form text. If None, no
-               notes will be stored. Defaults to None.
-        :param thumbnails: An optional list of full paths to thumbnail images.
-               There may be as many thumbnails as desired (for example: a full
-               turntable). The files MUST be named in the following format:
+        self.asset_obj = asset.Asset(language=language)
 
-               asset_name.####.ext
+    # --------------------------------------------------------------------------
+    def set_attributes(self,
+                       name,
+                       asset_parent_d,
+                       src_p=None,
+                       metadata=None,
+                       keywords=None,
+                       notes=None,
+                       thumbnails=None,
+                       poster_frame=None,
+                       merge=True,
+                       pins=None,
+                       verify_copy=False):
 
-               Where the asset_name is identical to the name of the asset_d dir,
-               the #### digits are frame numbers (required even if there is only
-               a single file), and ext is the file extension. If None, then no
-               thumbnails will be stored (but any previously stored will be
-               carried forward from the previous version as symlinks). Defaults
-               to None.
-        :param poster_frame: The frame number (as an integer or string that
-               evaluates to an integer) that indicates which frame of the
-               thumbnails to make the poster frame. If None, and thumbnails are
-               provided, then the first frame will be made into the poster.
-               Defaults to None.
-        :param merge: If True, then carry forward an files from the previous
-               version that were not explicitly added in this operation. This
-               allows for multiple publishes to layer together (i.e. publish
-               a model asset as version 1, and publish a material definition as
-               version 2 -> The model file(s) will be carried forward to version
-               2 as though they had been explicitly published a second time).
-               Defaults to True.
-        :param pins: A list of pins to set to point to the newly created
-               version. These are in addition to the automatic "LATEST" and
-               "CURRENT".
-        :param verify_copy: If True, then an md5 checksum will be done on each
-               source and each copy to ensure that the file was copied
-               correctly. Defaults to False.
-        :param language: The language used for communication with the end user.
-               Defaults to "english".
-
-        :return: Nothing.
-        """
-
-        self.asset_obj = asset.Asset(name=name,
-                                     asset_parent_d=asset_parent_d,
-                                     src_p=src_p,
-                                     metadata=metadata,
-                                     keywords=keywords,
-                                     notes=notes,
-                                     thumbnails=thumbnails,
-                                     merge=merge,
-                                     poster_frame=poster_frame,
-                                     pins=pins,
-                                     verify_copy=verify_copy,
-                                     language=language)
+        self.asset_obj.set_attributes(name=name,
+                                      asset_parent_d=asset_parent_d,
+                                      src_p=src_p,
+                                      metadata=metadata,
+                                      keywords=keywords,
+                                      notes=notes,
+                                      thumbnails=thumbnails,
+                                      merge=merge,
+                                      poster_frame=poster_frame,
+                                      pins=pins,
+                                      verify_copy=verify_copy)
 
     # --------------------------------------------------------------------------
     @staticmethod
